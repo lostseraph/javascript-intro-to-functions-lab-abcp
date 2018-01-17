388fcd3996f0b82df8de6e7723cb4a749e0e99a3
Roscoe Sabale <rsabale43@gmail.com>
2018-01-17 04:14:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
-function logwhisper(string) {
+function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-  if (string === lowercase) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
-  } else if (string === uppercase) {
+  } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   } else if (string == "I love you, Grandma") {
     return "I Love you, too."
